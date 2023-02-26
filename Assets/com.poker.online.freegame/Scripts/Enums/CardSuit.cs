f32e3c71792066895559a33f28f0148af51616b6
SGCEntertainment <smallgamescompany@gmail.com>
2023-02-26 12:30:13 +0300

    init

---
+++ Assets/com.poker.online.freegame/Scripts/Enums/CardSuit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public enum CardSuit
+{
+    [InspectorName("♦")]
+    B,
+
+    [InspectorName("♠")]
+    P,
+
+    [InspectorName("♣")]
+    T,
+
+    [InspectorName("♥")]
+    C
+}
---
+++ Assets/com.poker.online.freegame/Scripts/Enums/CardValue.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public enum CardValue
+{
+    [InspectorName("6")]
+    six,
+
+    [InspectorName("7")]
+    seven,
+
+    [InspectorName("8")]
+    eight,
+
+    [InspectorName("9")]
+    nine,
+
+    [InspectorName("10")]
+    ten,
+
+    [InspectorName("J")]
+    jack,
+
+    [InspectorName("D")]
+    queen,
+
+    [InspectorName("K")]
+    king,
+
+    [InspectorName("T")]
+    ace
+}
